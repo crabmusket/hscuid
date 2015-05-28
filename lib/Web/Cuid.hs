c4e90cfb0b0f5c512c524436cadab88fa6971c78
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-05-28 18:49:24 +1000

    Added global counter.
    And handed in my Haskell practicioners' card.

--- lib/Web/Cuid.hs
+++ lib/Web/Cuid.hs
@@ -2,8 +2,22 @@ module Web.Cuid (
     newCuid
 ) where
 
+import Data.IORef (IORef, newIORef, atomicModifyIORef')
 import Data.String (fromString)
 import Data.Text (Text)
+import System.IO.Unsafe (unsafePerformIO)
 
 newCuid :: IO Text
 newCuid = return (fromString "c")
+
+type Counter = IORef Int
+
+counter :: Counter
+counter = unsafePerformIO (newIORef 0)
+
+next :: Counter -> IO Int
+next c = atomicModifyIORef' c incrementAndWrap where
+    incrementAndWrap count = (succ count `mod` countMax, count)
+    countMax = base ^ blockSize
+    blockSize = 4
+    base = 36
