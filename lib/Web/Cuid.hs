58a20fa90b5efdc1599444d3aa289c964be7ce3d
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-05-29 00:00:28 +1000

    Add non-crypto random source.

--- lib/Web/Cuid.hs
+++ lib/Web/Cuid.hs
@@ -8,15 +8,18 @@ import Data.String (fromString)
 import Data.Text.Lazy (Text)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import System.IO.Unsafe (unsafePerformIO)
+import System.Random (randomRIO)
 
 import Formatting (Format, base, format, left, (%.))
 
 newCuid :: IO Text
-newCuid = concatIO [prefix, timestamp, globalCount] where
+newCuid = concatIO [prefix, timestamp, globalCount, random, random] where
     concatIO actions = fmap mconcat (sequence actions)
     prefix = return $ fromString "c"
-    timestamp = fmap (format number . round) getPOSIXTime
+    timestamp = fmap (format number . millis) getPOSIXTime
     globalCount = fmap (format numberPadded) (next counter)
+    random = fmap (format numberPadded) (randomRIO (0, maxValue))
+    millis posix = round $ posix * 1000
 
 type Counter = IORef Int
 
@@ -25,12 +28,12 @@ counter = unsafePerformIO (newIORef 0)
 
 next :: Counter -> IO Int
 next c = atomicModifyIORef' c incrementAndWrap where
-    incrementAndWrap count = (succ count `mod` countMax, count)
-    countMax = formatBase ^ blockSize
+    incrementAndWrap count = (succ count `mod` maxValue, count)
 
-formatBase, blockSize :: Int
+formatBase, blockSize, maxValue :: Int
 formatBase = 36
 blockSize = 4
+maxValue = formatBase ^ blockSize
 
 number, numberPadded :: Format Text (Int -> Text)
 (number, numberPadded) = (toBase, toBlockSize %. toBase) where
