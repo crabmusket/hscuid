0ccdc4e76718a13143a890cdb2a573e88a276e76
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-06-19 10:22:15 +1000

    Add another MonadIO action which creates a slug, as in the JS library.

--- lib/Web/Cuid.hs
+++ lib/Web/Cuid.hs
@@ -15,7 +15,7 @@ This module does not use crypto-strength sources of randomness. Use at your own
 peril!
 -}
 module Web.Cuid (
-    Cuid, newCuid
+    Cuid, newCuid, newSlug
 ) where
 
 import Control.Monad (liftM)
@@ -24,16 +24,15 @@ import Data.Char (ord)
 import Data.IORef (IORef, newIORef, atomicModifyIORef')
 import Data.String (fromString)
 import Data.Text (Text)
-import Data.Time.Clock.POSIX (getPOSIXTime)
-import Formatting (Format, base, fitRight, sformat, left, (%.))
+import Data.Time.Clock.POSIX (POSIXTime, getPOSIXTime)
+import Formatting (Format, base, fitLeft, fitRight, sformat, left, (%.))
 import Network.HostName (getHostName)
 import System.IO.Unsafe (unsafePerformIO)
 import System.Random (randomRIO)
 
-#if MIN_VERSION_base(4,8,0)
-import Data.Monoid ((<>))
-#else
-import Data.Monoid (mconcat, (<>))
+import Data.Monoid (Monoid, (<>))
+#if !MIN_VERSION_base(4,8,0)
+import Data.Monoid (mconcat)
 #endif
 
 #if defined(mingw32_HOST_OS)
@@ -62,20 +61,38 @@ newCuid = concatIO [c, time, count, fingerprint, random, random] where
     -- To avoid collisions between separate machines, generate a 'fingerprint'
     -- from details which are hopefully unique to this machine - PID and hostname.
     fingerprint = do
-        pid <- getPid
-        hostname <- getHostName
-        let hostSum = 36 + length hostname + sum (map ord hostname)
-        return (sformat twoOfNum pid <> sformat twoOfNum hostSum)
+        (pid, host) <- getFingerprint
+        return (sformat twoOfNum pid <> sformat twoOfNum host)
 
     -- And some randomness for good measure. Note that System.Random is not a
     -- source of crypto-strength randomness.
     random = liftM (sformat numberPadded) (randomRIO (0, maxCount))
 
-    -- Evaluate IO actions and concatenate their results.
-    concatIO actions = liftM mconcat (liftIO $ sequence actions)
+-- | Another convenience type. A Slug is not a Cuid.
+type Slug = Text
 
-    -- POSIX time library gives the result in fractional seconds.
-    millis posix = round (posix * 1000)
+-- | A slug is a shorter piece of text generated using some of the same
+-- techniques as CUIDs.
+newSlug :: MonadIO m => m Slug
+newSlug = concatIO [time, count, fingerprint, random] where
+    time = liftM (sformat twoOfNum . millis) getPOSIXTime
+    count = liftM (sformat numberPadded) (postIncrement counter)
+    random = liftM (sformat twoOfNum) (randomRIO (0, maxCount))
+    fingerprint = do
+        (pid, host) <- getFingerprint
+        return (sformat firstOfNum pid <> sformat lastOfNum host)
+
+-- A machine's fingerprint is derived from its PID and hostname.
+getFingerprint :: IO (Int, Int)
+getFingerprint = do
+    pid <- getPid
+    hostname <- getHostName
+    let hostSum = 36 + length hostname + sum (map ord hostname)
+    return (pid, hostSum)
+
+-- Evaluate IO actions and concatenate their results.
+concatIO :: (MonadIO m, Monoid a) => [IO a] -> m a
+concatIO actions = liftM mconcat (liftIO $ sequence actions)
 
 -- CUID calls for a globally incrementing counter per machine. This is ugly,
 -- but it satisfies the requirement.
@@ -97,10 +114,16 @@ blockSize = 4
 maxCount = formatBase ^ blockSize
 
 -- Number formatters for converting to the correct base and padding.
-number, numberPadded, twoOfNum :: Format Text (Int -> Text)
+number, numberPadded, twoOfNum, firstOfNum, lastOfNum :: Format Text (Int -> Text)
 number = base formatBase
 numberPadded = left blockSize '0' %. number
 twoOfNum = fitRight 2 %. number
+lastOfNum = fitRight 1 %. number
+firstOfNum = fitLeft 1 %. number
+
+-- POSIX time library gives the result in fractional seconds.
+millis :: POSIXTime -> Int
+millis posix = round (posix * 1000)
 
 -- Get the ID of the current process. This function has a platform-specific
 -- implementation. Fun times.
